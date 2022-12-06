2575fb5ab8a1c3d0326b1e1d6e77ab5020381600
Mike McGrath <mike@mikemcgrathonline.us>
2022-12-06 08:34:29 -0500

    socket.io stub

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import Container from "../../components/Container";
 import Heading from "../../components/Heading";
+
+let socket: any;
 
 export interface SignupPageProps {}
 
 const SignupPage = () => {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const socketInitializer = async () => {
+      await fetch("/api/socket");
+      socket = io();
+
+      socket.on("connect", () => {
+        console.log("connected");
+      });
+
+      socket.on("update-input", (msg: string) => {
+        setInput(msg);
+      });
+    };
+    socketInitializer();
+  }, []);
+
+  const onChangeHandler = (e: any) => {
+    setInput(e.target.value);
+    socket.emit("input-change", e.target.value);
+  };
+
   return (
     <Container>
       <Heading>Sign up</Heading>
+
+      <input
+        placeholder="Type something"
+        value={input}
+        onChange={onChangeHandler}
+      />
     </Container>
   );
 };
