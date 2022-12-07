c19041f57e5676928617569b3754a4623155f6fb
Mike McGrath <mike@mikemcgrathonline.us>
2022-12-07 07:12:43 -0500

    add in nav

---
+++ components/TopNav/index.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
+
+export interface TopNavProps {}
+
+const drawerWidth = 240;
+const navItems = [
+  { name: "Song List", path: "/" },
+  { name: "Song Queue", path: "/queue" },
+];
+
+const TopNav = () => {
+  const router = useRouter();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleClick = (path: string) => () => router.push(path);
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map(({ name, path }: any) => (
+          <ListItem key={name} disablePadding>
+            <ListItemButton
+              onClick={handleClick(path)}
+              sx={{ textAlign: "center" }}
+            >
+              <ListItemText primary={name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = global?.document?.body;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Garage Karaoke
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map(({ name, path }: any) => (
+              <Button
+                onClick={handleClick(path)}
+                key={name}
+                sx={{ color: "#fff" }}
+              >
+                {name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopNav;
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,12 +1,18 @@
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import React from "react";
+import TopNav from "../components/TopNav";
 
 export interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <Container maxWidth="xl">{children}</Container>;
+  return (
+    <Box>
+      <TopNav />
+      <Container maxWidth="xl">{children}</Container>
+    </Box>
+  );
 };
 
 export default MainLayout;
